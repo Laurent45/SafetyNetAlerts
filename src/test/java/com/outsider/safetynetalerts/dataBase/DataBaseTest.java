608502484f983997d8d11e88e28ee79a83249850
laurentfrederick <laurent.frederick@icloud.com>
2021-08-21 11:59:32 +0200

    create UT dataBase

--- src/test/java/com/outsider/safetynetalerts/dataBase/DataBaseTest.java
+++ src/test/java/com/outsider/safetynetalerts/dataBase/DataBaseTest.java
@@ -3,95 +3,67 @@ package com.outsider.safetynetalerts.dataBase;
 import com.outsider.safetynetalerts.model.FireStation;
 import com.outsider.safetynetalerts.model.MedicalRecord;
 import com.outsider.safetynetalerts.model.Person;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class DataBaseTest {
 
-    private DataBase dataBaseUT;
+    private DataBase databaseUT;
+    private Person person;
+    private FireStation fireStation;
+    private MedicalRecord medicalRecord;
 
     @BeforeEach
     void setUp() {
-        dataBaseUT = new DataBase();
-    }
-
-    @AfterEach
-    void tearDown() {
-        dataBaseUT = null;
-    }
-
-    @Test
-    void initShouldAddObjectIntoLists() throws IOException {
-        dataBaseUT.init();
-        assertThat(dataBaseUT.getPersonList().get(0).getFirstName())
-                .isEqualTo("John");
-        assertThat(dataBaseUT.getFireStationList().get(0).getAddress())
-                .isEqualTo("1509 Culver St");
-        assertThat(dataBaseUT.getMedicalRecordList().get(0).getLastName())
-                .isEqualTo("Boyd");
-    }
-
-    @Test
-    void createLinksShouldAddLinksBetweenLists() {
-        Person person = new Person();
-        person.setFirstName("Lo");
-        person.setLastName("Frazier");
-        person.setAddress("1, rue de Paris");
-        dataBaseUT.getPersonList().add(person);
-
-        MedicalRecord medicalRecord = new MedicalRecord();
-        medicalRecord.setFirstName("Lo");
-        medicalRecord.setLastName("Frazier");
-        dataBaseUT.getMedicalRecordList().add(medicalRecord);
-
-        FireStation fireStation = new FireStation();
-        fireStation.setAddress("1, rue de Paris");
-        dataBaseUT.getFireStationList().add(fireStation);
-
-        dataBaseUT.createLinks();
-        assertThat(dataBaseUT.getPersonList().get(0).getIdMedicalRecord())
-                .isEqualTo(medicalRecord.getIdMedicalRecord());
-        assertThat(dataBaseUT.getPersonList().get(0).getIdFireStations().get(0))
-                .isEqualTo(fireStation.getIdFireStation());
-        assertThat(dataBaseUT.getMedicalRecordList().get(0).getIdPerson())
-                .isEqualTo(person.getIdPerson());
-        assertThat(dataBaseUT.getFireStationList().get(0).getIdPersons().get(0))
-                .isEqualTo(person.getIdPerson());
+        databaseUT = new DataBase();
+        person = new Person();
+        person.setFirstName("John");
+        person.setLastName("Boyd");
+        person.setAddress("1509 Culver St");
+        person.setCity("Culver");
+        person.setZip("97451");
+        person.setEmail("jaboyd@email.com");
+        person.setPhone("841-874-6512");
+        fireStation = new FireStation();
+        fireStation.setStation(3);
+        fireStation.setAddress("1509 Culver St");
+        medicalRecord = new MedicalRecord();
+        medicalRecord.setFirstName("John");
+        medicalRecord.setLastName("Boyd");
+        medicalRecord.setBirthdate("03/06/1984");
+        medicalRecord.setMedications(List.of("aznol:350mg", "hydrapermazol" +
+            ":100mg"));
+        medicalRecord.setAllergies(List.of("nillacilan"));
     }
 
     @Test
-    void getPersonShouldReturnAnOptionalNotEmpty() {
-        Person person = new Person();
-        person.setFirstName("Lo");
-        person.setLastName("Frazier");
-        person.setAddress("1, rue de Paris");
-        dataBaseUT.getPersonList().add(person);
-        Optional<Person> personOptional = dataBaseUT.getPerson("Lo", "Frazier");
-        assertThat(personOptional.isEmpty()).isFalse();
+    void whenInit_thenAddObjectsInLists() throws IOException {
+        databaseUT.init();
+        assertThat(databaseUT.getPersonList().get(0)).isEqualTo(person);
+        assertThat(databaseUT.getFireStationList().get(0)).isEqualTo(fireStation);
+        assertThat(databaseUT.getMedicalRecordList().get(0)).isEqualTo(medicalRecord);
     }
 
     @Test
-    void getIdPersonsShouldReturnAnListOfTwoIds() {
-        Person p1 = new Person();
-        p1.setFirstName("Lo");
-        p1.setLastName("Frazier");
-        p1.setAddress("1, rue de Paris");
-        Person p2 = new Person();
-        p2.setFirstName("James");
-        p2.setLastName("Frazier");
-        p2.setAddress("1, rue de Paris");
-        dataBaseUT.getPersonList().add(p1);
-        dataBaseUT.getPersonList().add(p1);
-        assertThat(dataBaseUT.getIdPersons("1, rue de Paris").size())
-                .isEqualTo(2);
+    void testCreateLinksBetweenPersonAndMedicalRecord() {
+        databaseUT.getPersonList().add(person);
+        databaseUT.getMedicalRecordList().add(medicalRecord);
+        databaseUT.createLinksBetweenPersonAndMedicalRecord();
+        assertThat(databaseUT.getPersonList().get(0).getMedicalRecord()).isEqualTo(medicalRecord);
+        assertThat(databaseUT.getMedicalRecordList().get(0).getPerson()).isEqualTo(person);
+    }
 
-
+    @Test
+    void testCreateLinksBetweenPersonAndFireStation() {
+        databaseUT.getPersonList().add(person);
+        databaseUT.getFireStationList().add(fireStation);
+        databaseUT.createLinksBetweenPersonAndFireStation();
+        assertThat(databaseUT.getPersonList().get(0).getFireStations().get(0)).isEqualTo(fireStation);
+        assertThat(databaseUT.getFireStationList().get(0).getPersons().get(0)).isEqualTo(person);
     }
 }
